2d1902dc66f1114017c20909163909b46b7fc79c
mackliu <macklun@gmail.com>
2025-12-29 16:05:18 +0800

    完成管理登入及登出功能

---
+++ api/login.php
@@ -0,0 +1,15 @@
+<?php 
+include_once "db.php";
+
+$chk=$Admin->count($_POST);
+
+if($chk){
+    $_SESSION['admin']=$_POST['acc'];
+    to("../back.php");
+}
+?>
+<script>
+    alert("帳號或密碼錯誤");
+    location.href="../login.php";
+</script>
+
