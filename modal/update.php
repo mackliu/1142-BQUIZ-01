312829d33e689663d81399351cc47986a549070a
mackliu <macklun@gmail.com>
2025-12-29 10:48:14 +0800

    整合上傳更新圖片的三個modal 為一個檔案

--- modal/update.php
+++ modal/update.php
@@ -1,9 +1,26 @@
-<div class="cent">更新圖片</div>
+<?php
+switch($_GET['table']){
+    case "title":
+        $title="標題區圖片";
+        $header="更新圖片";
+        break;
+    case "image":
+        $title="校園映像圖片";
+        $header="更換圖片";
+        break;
+    case "mvim":
+        $title="動畫圖片";
+        $header="更換動畫";
+        break;
+
+}
+?>
+<div class="cent"><?=$header;?></div>
 <hr>
 <form action="./api/update.php?table=<?=$_GET['table'];?>" method="post" enctype="multipart/form-data">
     <table style="width:70%;margin:auto">
         <tr>
-            <td>標題區圖片</td>
+            <td><?=$title;?></td>
             <td><input type="file" name="img" id=""></td>
         </tr>
         <tr>
