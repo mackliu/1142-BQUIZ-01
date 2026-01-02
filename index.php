784733f750ba10855a5ad5a51a2357dd67caf57e
mackliu <macklun@gmail.com>
2026-01-02 14:12:26 +0800

    完成校園映像圖片顯示及切換功能

--- index.php
+++ index.php
@@ -12,6 +12,12 @@
     <link href="./css/css.css" rel="stylesheet" type="text/css">
     <script src="./js/jquery-3.4.1.js"></script>
     <script src="./js/js.js"></script>
+    <style>
+    .btn {
+        text-align: center;
+        margin: 5px auto;
+    }
+    </style>
 </head>
 
 <body>
@@ -81,16 +87,37 @@
                     onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
                 <div style="width:89%; height:480px;" class="dbor">
                     <span class="t botli">校園映象區</span>
+                    <div class='btn' onclick="pp(1)">
+                        <img src="icon/up.jpg" alt="" srcset="">
+                    </div>
+                    <div>
+
+                    <?php 
+                       $images=$Image->all(['sh'=>1]);
+                       foreach($images as $key => $img){
+                        echo "<div class='im' id='ssaa{$key}' style='display:none;text-align:center;margin:3px 0'>";
+                        echo "<img src='upload/{$img['img']}' style='width:150px;height:103px;border:3px solid orange;'>";
+
+                        echo "</div>";
+                       }
+
+
+                    ?>
+
+                    </div>
+                    <div  class='btn' onclick="pp(2)">
+                        <img src="icon/dn.jpg" alt="" srcset="">
+                    </div>
                     <script>
                     var nowpage = 0,
-                        num = 0;
+                        num = <?=count($images)?>;
 
                     function pp(x) {
                         var s, t;
                         if (x == 1 && nowpage - 1 >= 0) {
                             nowpage--;
                         }
-                        if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+                        if (x == 2 && (nowpage + 1) <= num * 1 - 3) {
                             nowpage++;
                         }
                         $(".im").hide()
@@ -98,7 +125,13 @@
                             t = s * 1 + nowpage * 1;
                             $("#ssaa" + t).show()
                         }
+                       // console.log(x, nowpage);
                     }
+                    /* s   t   nowpage
+                    0   0*1 + 0*1  0   #ssaa0
+                    1   1*1 + 0*1  2   #ssaa1
+                    2   2*1 + 0*1  2   #ssaa2 */
+
                     pp(1)
                     </script>
                 </div>
