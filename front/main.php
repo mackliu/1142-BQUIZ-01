213a00f50a8a948682c3483601fa3e9816d66861
mackliu <macklun@gmail.com>
2026-01-02 15:23:02 +0800

    完成首頁最新消息列表及彈出視窗詳情內容

--- front/main.php
+++ front/main.php
@@ -61,8 +61,9 @@
                             foreach($news as $n){
                                 echo "<li>";
                                 echo mb_substr($n['text'],0,20);
-
-
+                                echo "<div class='all' style='display:none;'>";
+                                echo $n['text'];
+                                echo "</div>";
                                 echo "</li>";
                             }
 
@@ -70,6 +71,7 @@
                     </ul>
                     <div id="altt"
                         style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+                   
                     </div>
                     <script>
                     $(".ssaa li").hover(
