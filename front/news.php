087b0b4ea5459ec949a5d2830ea6c1debfcd102f
mackliu <macklun@gmail.com>
2026-01-02 15:40:33 +0800

    建立更多最新消息顯示及分頁功能

--- front/news.php
+++ front/news.php
@@ -4,8 +4,49 @@
 	</marquee>
 	<div style="height:32px; display:block;"></div>
 	<!--正中央-->
+	<h3>更多最新消息最顯示區</h3>
+	<hr>
+	<?php 
+        $total=$News->count(['sh'=>1]);
+        $div=5;
+        $pages=ceil($total/$div);
+        $now=$_GET['p']??1;
+        $start=($now-1)*$div;
+        $news=$News->all(['sh'=>1]," limit $start,$div");	
+		
+	?>
+	<ol class="ssaa" start="<?=$start+1;?>" style="list-style-type:decimal;">
+		<?php 
+			foreach($news as $n){
+				echo "<li>";
+				echo mb_substr($n['text'],0,20);
+				echo "<div class='all' style='display:none;'>";
+				echo $n['text'];
+				echo "</div>";
+				echo "</li>";
+			}
+
+		?>
+	</ol>
+
+
+
 	<div style="text-align:center;">
-		<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-		<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
+            <?php 
+                if($now>1){
+                    $prev=$now-1;
+                    echo "<a class='bl' href='?do=$do&p=$prev'> < </a>";
+                }
+            
+                for($i=1;$i<=$pages;$i++){
+                    $size=($i==$now)?"24px":"16px";
+                    echo "<a href='?do=$do&p=$i' style='font-size:$size;'> $i </a>";
+                 }
+                if($now<$pages){
+                    $next=$now+1;
+                    echo "<a class='bl' href='?do=$do&p=$next'> > </a>";
+                }
+            ?>
+
 	</div>
 </div>
