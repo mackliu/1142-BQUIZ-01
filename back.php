a03a01a4ae3b0d3419bc77b4502a9303bb6bd862
mackliu <macklun@gmail.com>
2026-01-02 09:25:24 +0800

    完成頁尾版權顯示

--- back.php
+++ back.php
@@ -146,7 +146,12 @@ if(!isset($_SESSION['admin'])){
         <div style="clear:both;"></div>
         <div
             style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-            <span class="t" style="line-height:123px;"></span>
+            <span class="t" style="line-height:123px;">
+                <?php 
+                    $bottom=$Bottom->find(1);
+                    echo $bottom['bottom'];
+                ?>
+            </span>
         </div>
     </div>
 
